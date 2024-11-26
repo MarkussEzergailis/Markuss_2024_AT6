08f0a493781d3417ed9a936fc0213d9059748840
Markuss Ezergailis <ezergailis.markuss@gmail.com>
2024-11-26 16:39:14 +0100

    Second Commit: Added Extra.py (With 2 instances of it) + extra.txt + gitignore

---
+++ extra.py
@@ -0,0 +1,7 @@
+from InputValidator import UserInputValidator
+
+list2 = [1,2,11,-1,-2,-4,4,-33,6,11,-2]
+list1 = [1,2,22,-1,-66,-3,4,5,6,7,-2]
+
+validator1 = UserInputValidator(list1)
+validator2 = UserInputValidator(list2)

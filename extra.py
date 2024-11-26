3efc7e4a000f889cff660dff8d02e05a81e6166e
Markuss Ezergailis <ezergailis.markuss@gmail.com>
2024-11-26 16:41:45 +0100

    Third Commit: Called both instances from the Second Commit

--- extra.py
+++ extra.py
@@ -5,3 +5,6 @@ list1 = [1,2,22,-1,-66,-3,4,5,6,7,-2]
 
 validator1 = UserInputValidator(list1)
 validator2 = UserInputValidator(list2)
+
+validator1.validate_positive_integer()
+validator2.validate_positive_integer()

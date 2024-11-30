f17e86ab6cad23b4da9628ea51b641530b65b86c
Markuss Ezergailis <ezergailis.markuss@gmail.com>
2024-11-30 18:12:05 +0100

    Third Commit: Calling both instances

--- InputValidator_extra.py
+++ InputValidator_extra.py
@@ -5,3 +5,6 @@ list2 = [1,9,-3,-1,-2,-3,4,5,2,7,-2]
 
 instance1 = UserInputValidator(list1)
 instance2 = UserInputValidator(list2)
+
+instance1.validate_positive_integer()
+instance2.validate_positive_integer()

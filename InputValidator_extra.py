9878aea4830682c0fcb1711a2a39632bf2a3068b
Markuss Ezergailis <ezergailis.markuss@gmail.com>
2024-11-30 18:05:13 +0100

    Second Commit: added 2 instances + extra.txt + gitignore

---
+++ InputValidator_extra.py
@@ -0,0 +1,7 @@
+from InputValidator import UserInputValidator
+
+list1 = [1,2,4,-1,-2,-3,4,5,-6,1,-2]
+list2 = [1,9,-3,-1,-2,-3,4,5,2,7,-2]
+
+instance1 = UserInputValidator(list1)
+instance2 = UserInputValidator(list2)
